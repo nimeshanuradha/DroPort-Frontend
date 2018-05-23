dae3a8dc10929bbf4b8533cbc8da5ce7cb85b4ce
Kaushalya Prasadini <madhavikau@gmail.com>
2018-05-23 22:21:53 +0530

    Owner UIs

---
+++ droport- frontend/src/app/view/owner/drone-reg/drone-reg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-drone-reg',
+  templateUrl: './drone-reg.component.html',
+  styleUrls: ['./drone-reg.component.css']
+})
+export class DroneRegComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
