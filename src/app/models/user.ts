6abca66ff844fb377b416adcdbecb1709caf048f
Nimesh Anuradha <>
2018-06-19 17:27:14 +0530

    Chnage repo to DroPort-Frontend

---
+++ src/app/models/user.ts
@@ -0,0 +1,71 @@
+export interface Admin{
+    adm_id:number
+    f_name: string
+    l_name: string
+    dob:Date
+    nic:any
+    age:number
+    address: any[]
+    phone_mobile:number	
+    phone_fix: number
+    gender:string
+    email:string
+    fb_profile_link:any
+    username:string
+    password:any
+
+}
+
+export interface Customer{
+    cus_id:number
+    f_name: string
+    l_name: string
+    dob:Date
+    nic:any
+    age:number
+    address: any[]
+    phone_mobile:number	
+    phone_fix: number
+    gender:string
+    email:string
+    fb_profile_link:any
+    username:string
+    password:any
+
+}
+
+export interface Pilot{
+    adm_id:number
+    f_name: string
+    l_name: string
+    dob:Date
+    nic:any
+    age:number
+    address: any[]
+    phone_mobile:number	
+    phone_fix: number
+    gender:string
+    email:string
+    fb_profile_link:any
+    username:string
+    password:any
+
+}
+
+export interface Owner{
+    own_id:number
+    f_name: string
+    l_name: string
+    dob:Date
+    nic:any
+    age:number
+    address: any[]
+    phone_mobile:number	
+    phone_fix: number
+    gender:string
+    email:string
+    fb_profile_link:any
+    username:string
+    password:any
+
+}
