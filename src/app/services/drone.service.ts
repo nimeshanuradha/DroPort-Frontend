41cd0c2517b6216719c2d9f6b2e7b60e28512a7f
Prabhath Iroshan <Prabhath Iroshan>
2018-08-19 09:26:17 +0530

    modified dorne reg page and customer new req page

--- src/app/services/drone.service.ts
+++ src/app/services/drone.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http,Response } from '@angular/http';
 import { BehaviorSubject } from "rxjs/BehaviorSubject";
 
 
@@ -16,6 +16,14 @@ export class DroneService {
   getAll_Drones(){
     return this.http.get('http://localhost:3000/api/drn/all').map(res=>res.json())
   }
+
+
+    //POST New Drone
+  post_NewDrone(drone_data){
+      console.log("post_NewDrone called")
+      return this.http.post("http://localhost:3000/api/drn/new_drone",drone_data).map((res:Response)=>res.json())
+    }
+      
 
 
 
--- src/app/view/owner/drone-reg/drone-reg.component.ts
+++ src/app/view/owner/drone-reg/drone-reg.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DroneService} from '../../../services/drone.service';
 
 @Component({
   selector: 'app-drone-reg',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DroneRegComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public router: Router,
+    public DroneService: DroneService,
+
+  ) { }
 
   ngOnInit() {
+  }
+
+  get_new_drone(new_drone_reg: NgForm){
+    console.log('sending :', new_drone_reg.value)
+    var drone_data = new_drone_reg.value
+    
+    this.DroneService.post_NewDrone(drone_data).subscribe(res=>console.log("Successfully added"))
+
   }
 
 }
